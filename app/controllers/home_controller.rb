b8542630fd880bd17c20aab61a7b2a5de077f14c
Di <you@example.com>
2021-02-04 19:27:23 +0200

    devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,16 @@
 class HomeController < ApplicationController
+	before_action :redirect_when_logged_in
 
 	def index
 		
 	end
+
+	def redirect_when_logged_in
+		if doctor_signed_in?
+			redirect_to edit_doctor_registration_path
+		elsif patient_signed_in?
+			redirect_to edit_patient_registration_path
+		end		
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  devise_for :doctors, path: 'doctors', controllers: {
+        sessions: 'doctors/sessions'#,
+        #confirmations: 'doctors/confirmations',
+        #passwords: 'doctors/passwords',
+        #registrations: 'doctors/registrations',
+        #unlocks: 'doctors/unlocks',
+        #omniauth_callbacks: 'doctors/omniauth_callbacks'
+      }
+  devise_for :patients, path: 'patients', controllers: {
+        sessions: 'patients/sessions'#,
+        #confirmations: 'patients/confirmations',
+        #passwords: 'patients/passwords',
+        #registrations: 'patients/registrations',
+        #unlocks: 'patients/unlocks',
+        #omniauth_callbacks: 'patients/omniauth_callbacks'
+      }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
 end
