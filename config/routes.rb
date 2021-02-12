97920c6ab83afcccb237deeebc423e69d4cd41d0
Di <you@example.com>
2021-02-12 21:45:17 +0200

    added admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,14 @@ Rails.application.routes.draw do
   match '/doctors', to: 'doctors#index', via: 'get'
   match '/doctors/search', to: 'doctors#search', via: 'get'
   match '/doctors/profile/:id', to: 'doctors#show', via: 'get'
+  devise_for :admins, path: 'admins', controllers: {
+        sessions: 'admins/sessions'
+      }
   devise_for :doctors, path: 'doctors', controllers: {
-        sessions: 'doctors/sessions'#,
-        #confirmations: 'doctors/confirmations',
-        #passwords: 'doctors/passwords',
-        #registrations: 'doctors/registrations',
-        #unlocks: 'doctors/unlocks',
-        #omniauth_callbacks: 'doctors/omniauth_callbacks'
+        sessions: 'doctors/sessions'
       }
   devise_for :patients, path: 'patients', controllers: {
-        sessions: 'patients/sessions'#,
-        #confirmations: 'patients/confirmations',
-        #passwords: 'patients/passwords',
-        #registrations: 'patients/registrations',
-        #unlocks: 'patients/unlocks',
-        #omniauth_callbacks: 'patients/omniauth_callbacks'
+        sessions: 'patients/sessions'
       }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
